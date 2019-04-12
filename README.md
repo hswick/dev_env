3532ab1576f72961f452d0d614dcd82d81f0a9a0
Harley Swick <hdswick@gmail.com>
2019-04-11 19:04:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# dev_env
+
+Script for initializing my personal development environment on Debian based Linux machines.
+
+## Packages
+
+* emacs
+* ruby
+* golang
+* rust
+* nodejs
+* elm
+
+# Installation
+
+```
+./setup.sh
+```
---
+++ setup.sh
@@ -0,0 +1,39 @@
+apt update && apt upgrade
+
+# Emacs installation
+apt install emacs -y
+echo '\n' >> $HOME/.bashrc
+echo 'alias e="emacs -nw"' >> $HOME/.bashrc
+echo '\n' >> $HOME/.bashrc
+
+# Ruby
+apt install ruby -y
+
+# Postgres
+apt install postgresql postgresql-client -y
+
+# Go installation
+wget https://dl.google.com/go/go1.12.3.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.12.3.linux-amd64.tar.gz
+echo '\n' >> $HOME/.bashrc
+echo '# Golang env' >> $HOME/.bashrc
+echo 'export PATH=$PATH:/usr/local/go/bin' >> $HOME/.bashrc
+echo '\n' >> $HOME/.bashrc
+
+# Nodejs installation
+curl -L https://git.io/n-install -y
+source $HOME/.bashrc
+n stable
+
+# Elm installation
+npm install -g elm
+
+# Rust installation
+curl https://sh.rustup.rs -sSf | sh
+echo '\n' >> $HOME/.bashrc
+echo '# Rust env' >> $HOME/.bashrc
+echo 'export PATH=$PATH:$HOME/.cargo/bin' >> $HOME/.bashrc
+echo '\n' >> $HOME/.bashrc
+
+# Refresh bash after changes
+source $HOME/.bashrc
