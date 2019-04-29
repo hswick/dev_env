8ec60c1c4e1b3d75c4b5367b4581a96a354088c9
Harley Swick <hdswick@gmail.com>
2019-04-29 11:54:31 -0500

    Changing setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,7 @@ apt update && apt upgrade
 
 # Emacs installation
 apt install emacs -y
-echo '\n' >> $HOME/.bashrc
 echo 'alias e="emacs -nw"' >> $HOME/.bashrc
-echo '\n' >> $HOME/.bashrc
 cp ./.emacs $HOME/.
 cp ./mistyday-theme.el $HOME/.emacs.d/.
 
@@ -17,10 +15,8 @@ apt install postgresql postgresql-client -y
 # Go installation
 wget https://dl.google.com/go/go1.12.3.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.12.3.linux-amd64.tar.gz
-echo '\n' >> $HOME/.bashrc
 echo '# Golang env' >> $HOME/.bashrc
 echo 'export PATH=$PATH:/usr/local/go/bin' >> $HOME/.bashrc
-echo '\n' >> $HOME/.bashrc
 
 # Nodejs installation
 curl -L https://git.io/n-install -y
@@ -32,10 +28,8 @@ npm install -g elm
 
 # Rust installation
 curl https://sh.rustup.rs -sSf | sh
-echo '\n' >> $HOME/.bashrc
 echo '# Rust env' >> $HOME/.bashrc
 echo 'export PATH=$PATH:$HOME/.cargo/bin' >> $HOME/.bashrc
-echo '\n' >> $HOME/.bashrc
 
 # Refresh bash after changes
 source $HOME/.bashrc
