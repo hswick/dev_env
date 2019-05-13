d4415629027a690275dea2a70c50e265b2d2103f
Harley Swick <hdswick@gmail.com>
2019-05-13 00:05:03 -0500

    Commented out go and node, added python install

--- setup.sh
+++ setup.sh
@@ -20,19 +20,22 @@ echo 'export PATH=$PATH:$HOME/.cargo/bin' >> $BASE
 # Ruby
 sudo apt install ruby -y
 
+# Python
+sudo apt install pipenv
+
 # Go installation
-wget https://dl.google.com/go/go1.12.3.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.12.3.linux-amd64.tar.gz
-echo '# Golang env' >> $BASE
-echo 'export PATH=$PATH:/usr/local/go/bin' >> $BASE
+#wget https://dl.google.com/go/go1.12.3.linux-amd64.tar.gz
+#tar -C /usr/local -xzf go1.12.3.linux-amd64.tar.gz
+#echo '# Golang env' >> $BASE
+#echo 'export PATH=$PATH:/usr/local/go/bin' >> $BASE
 
 # Nodejs installation
-curl -L https://git.io/n-install -y
-source $BASE
-n stable
+#curl -L https://git.io/n-install -y
+#source $BASE
+#n stable
 
 # Elm installation
-npm install -g elm
+#npm install -g elm
 
 # Refresh bash after changes
-source BASE
+source $BASE
